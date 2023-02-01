25e8ddf173e5213cc33dd68a2e7e52a66d458eab
gucabal <cabralalvesgustavo@gmail.com>
2023-02-01 18:28:31 -0300

    Primeiros exercicios de Loop, utilizando for e while

--- src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
+++ src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
@@ -8,5 +8,32 @@ e informe o maior número
 e a média desses números.
 */
 public class Ex3_MaiorEMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int media = 0;
+
+
+        int count = 0;
+        do{
+            System.out.println("Numero: ");
+            numero = scan.nextInt();
+
+            if (numero > maior) maior = numero;
+            media = media + numero;
+
+            count = count + 1;
+        } while(count < 5);
+
+        System.out.println("O numero maior é " + maior);
+        System.out.println("A média é " + (media/5));
+
+
+
+    }
+
+
 
 }
--- src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
+++ src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
@@ -8,5 +8,33 @@ calcule e mostre a quantidade de números pares
 e a quantidade de números impares.
 */
 public class Ex4_ParEImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        int numero;
+        int count = 0;
+        int count2 = 0;
+        int quantPares = 0;
+        int quantImpares = 0;
+
+
+        System.out.println("Digite a quantidade de números a serem inseridos");
+        count = scan.nextInt();
+
+        while (count > count2){
+            System.out.println("Dgite o número: ");
+            numero = scan.nextInt();
+
+            if(numero % 2 == 0) quantPares++;
+            else quantImpares++;
+
+            count2++;
+        }
+
+        System.out.println("Quantidade de números pares: " + quantPares);
+        System.out.println("Quantidade de números impares: " + quantImpares);
+
+
+
+    }
 }
